b8918a3eaeca0d53e37fe84f098e56c8e4c87b58
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2024-05-11 05:57:00 +0700

    UPDATE: Add fetch user

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('products', [\App\Http\Controllers\API\ProductController::class, 'all']);
 Route::get('categories', [\App\Http\Controllers\API\ProductCategoryController::class, 'all']);
 
 Route::post('register', [\App\Http\Controllers\API\UserController::class, 'register']);
 Route::post('login', [\App\Http\Controllers\API\UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [\App\Http\Controllers\API\UserController::class, 'fetch']);
+});
